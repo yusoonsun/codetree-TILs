512f5e4f30ddf1fbec02316889fc58e5089297d3
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-07 17:02:59 +0900

    Title: 달에서 무게 구하기, Time: 95ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240107/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int A = 13;
+        double B = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f", A, B, A * B);
+    }
+}
