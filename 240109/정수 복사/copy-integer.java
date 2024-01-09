28e9b64a0f4fdf3fc67298f90f13c28fd0197874
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-09 20:53:04 +0900

    Title: 정수 복사, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240109/정수 복사/copy-integer.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        b = a;
+
+        int c = a * b;
+
+        System.out.println(a + " " + b);
+        System.out.println(c);
+    }
+}
