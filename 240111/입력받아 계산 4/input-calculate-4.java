7dc40a5171c1681a614a0a905c26c53b8152cf2a
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-11 20:26:29 +0900

    Title: 입력받아 계산 4, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240111/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+        System.out.print(a * 2);
+    }
+}
