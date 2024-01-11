5bcb9ff5e5fd04ceaba474b2927f25768bb1ecca
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-11 20:07:14 +0900

    Title: 변수 값 복사하기, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240111/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 3;
+
+        a = b = c;
+        System.out.println(a + " " + b + " " + c);
+    }
+}
