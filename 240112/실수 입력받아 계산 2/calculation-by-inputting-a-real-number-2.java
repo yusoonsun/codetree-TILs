63f7f25785a4c5e724df4c1576211ccbdc5b35f9
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-12 10:31:48 +0900

    Title: 실수 입력받아 계산 2, Time: 156ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240112/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a;
+        a = sc.nextDouble();
+
+        System.out.printf("%.2f", a + 1.5);
+    }
+}
