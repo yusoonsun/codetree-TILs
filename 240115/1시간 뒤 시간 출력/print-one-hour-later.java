781a5f80464abeb2921297d968cd8498f79103fb
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-15 19:19:29 +0900

    Title: 1시간 뒤 시간 출력, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240115/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(":"); // : 기준으로 잘라서 입력 받겠다는 뜻
+        int h = sc.nextInt(); // : 기호가 나오기 전까지 입력 진행
+        int m = sc.nextInt(); // : 기호가 나오기 전 or 입력의 마지막 부분까지 진행
+        System.out.println((h + 1) + ":" + m);
+    }
+}
