850381a6ba8386bdfaa02bcf04ef025a0aa01bcb
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-16 19:04:26 +0900

    Title: 날짜 변경하여 출력 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240116/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int mm = sc.nextInt();
+        int dd = sc.nextInt();
+        int yyyy = sc.nextInt();
+        System.out.println(yyyy + "." + mm + "." + dd);
+    }
+}
