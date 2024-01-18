2d2222f6c6f7463ebda3d93978bc1773410d8bb2
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-18 19:42:56 +0900

    Title: 시험 통과 여부 확인하기, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240118/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        if(a >= 80) {
+            System.out.println("pass");
+        }
+        else {
+            System.out.println(" more score");
+        }
+            
+
+    }
+}
