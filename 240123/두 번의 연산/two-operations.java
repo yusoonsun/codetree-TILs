341910074c00da2d7d395fb0f65393264cf8eb8d
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-23 23:29:59 +0900

    Title: 두 번의 연산, Time: 190ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240123/두 번의 연산/two-operations.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if(a%2!=0)
+            a+=3;
+        if(a%3==0)
+            a/=3;
+        System.out.println(a);
+    }
+}
