00bc7d94b89e2d9be92d8cb380b8f54dd130d114
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-23 23:23:34 +0900

    Title: 최대 2번의 연산, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240123/최대 2번의 연산/up-to-2-calculations.java
+++ 240123/최대 2번의 연산/up-to-2-calculations.java
@@ -12,7 +12,8 @@ public class Main {
             a = (a / 2);
         }
         if (a % 2 != 0) {
-            System.out.println((a + 1) / 2);
+            a = (a + 1) / 2;
         }
+        System.out.println(a);
     }
 }
