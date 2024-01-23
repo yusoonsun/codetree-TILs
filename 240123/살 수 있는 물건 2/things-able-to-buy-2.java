e3c195b4990c10683a69240188bde02e51ec80df
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-23 23:05:16 +0900

    Title: 살 수 있는 물건 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240123/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n >= 3000)
+            System.out.println("book");
+        else if (n >= 1000 && n < 3000)
+            System.out.println("mask");
+        else if (n >= 500 && n < 1000)
+            System.out.println("pen");
+        else
+            System.out.println("no");
+    }
+}
