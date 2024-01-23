1fbdbf9784080c10338c67051623b5baa172387e
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-23 22:42:12 +0900

    Title: 출석 부르기, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240123/출석 부르기/calling-attendance.java
+++ 240123/출석 부르기/calling-attendance.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
+        int a = sc.nextInt();
 
         if (a == 1) {
             System.out.println("John");
