0b2a50784c96fd7046a276c90f71264242f59032
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-24 19:03:22 +0900

    Title: 점수 비교, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240124/점수 비교/score-comparison.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int AM = sc.nextInt();
+        int AE = sc.nextInt();
+        int BM = sc.nextInt();
+        int BE = sc.nextInt();
+
+        if(AM>=BM&&AE>=BE)
+            System.out.println(1);
+        else
+            System.out.print(0);
+    }
+}
