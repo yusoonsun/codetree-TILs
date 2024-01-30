343a3192d0c71e7eff4d9d348f2e7717b8d7e6d5
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-30 19:15:31 +0900

    Title: 좀 더 어려운 수학 점수, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240130/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
+++ 240130/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -4,18 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
+        int Ma = sc.nextInt();
+        int Ea = sc.nextInt();
+        int Mb = sc.nextInt();
+        int Eb = sc.nextInt();
 
-        if(a==c)
-            if(b>d)
-            System.out.println("A");
-            else
-            System.out.println("B");
-
-        else if(a>c)
+        if(Ma>Mb || Ma==Mb && Ea>Eb)
             System.out.println("A");
         else
             System.out.println("B");
