725a1301f38a17dbcdce5db79720df0865da0d5e
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-30 18:43:33 +0900

    Title: 세 수의 중간값, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240130/세 수의 중간값/median-of-three-numbers.java
+++ 240130/세 수의 중간값/median-of-three-numbers.java
@@ -8,7 +8,7 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if(b>=a && b<=c)
+        if(b>=a && b<c)
             System.out.println(1);
         else
             System.out.println(0);
