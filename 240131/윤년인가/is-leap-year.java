dea7fd62a8282eb8e743953b004b87f8ef41eaab
yusoonsun <150895691+yusoonsun@users.noreply.github.com>
2024-01-31 20:11:25 +0900

    Title: 윤년인가, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240131/윤년인가/is-leap-year.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+
+        if((y%4==0 && y%100==0) || (y%4==0 && y%100==0 && y%400==0))
+            System.out.println("false");
+        else
+            System.out.println("true");
+    }
+}
